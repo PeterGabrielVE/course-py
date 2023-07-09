d57536d80622a569acf0c1762d83e07360ea8954
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-09 13:06:12 -0400

    metodos strings

---
+++ tipos/04-metodos-strings.py
@@ -0,0 +1,12 @@
+animal = "  piolin VUELA  "
+
+print(animal.upper())
+print(animal.lower())
+print(animal.capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.strip().capitalize())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("lin"))
+print(animal.replace("VUELA","CAMINA"))
