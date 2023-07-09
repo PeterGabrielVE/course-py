8e2395cca9224bfedabbd2cd164fc3784fa095a1
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-09 13:31:39 -0400

    modulos math

---
+++ 07-numeros-funciones.py
@@ -0,0 +1,13 @@
+import math
+
+print(round(1.3))
+print(round(1.7))
+print(round(1.4))
+print(abs(1.3))
+
+print(math.ceil(1.2))
+print(math.floor(1.99999))
+print(math.isnan(23))
+#print(math.isnan("23"))
+print(math.pow(10,3))
+print(math.sqrt(225))
