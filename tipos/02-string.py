cfe347f1e0064c68f36c8846b541731f7dff9fdb
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-08 21:11:31 -0400

    string

---
+++ tipos/02-string.py
@@ -0,0 +1,8 @@
+nombre_curso = "Ultimate python"
+descripcion_curso = """
+Ultimate Python,
+este curso promete todos los detalles para ser un buen programador
+"""
+
+print(len(nombre_curso))
+print(nombre_curso[0:8])
