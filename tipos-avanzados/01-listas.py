bbdf7a988963d6c46add2b2161ba0277e14b04b7
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-23 08:55:46 -0400

    listas

---
+++ tipos-avanzados/01-listas.py
@@ -0,0 +1,8 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["chanchito", "feliz"]
+booleans = [True, False, True, True]
+matriz = [[0,1], [1,0]]
+ceros = [0] * 10
+rango = list(range(10))
+print(rango)
