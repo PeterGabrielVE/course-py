b2d3ed7d95fbe495136b93177f6dfb0ca50d806e
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-23 09:34:23 -0400

    iterar

---
+++ tipos-avanzados/04-iterar-listas.py
@@ -0,0 +1,10 @@
+mascotas = ["Pelusa","Terry","Luna","Pluton"]
+
+for mascota in mascotas:
+    print(mascota)
+
+for mascota in enumerate(mascotas):
+    print(mascota)
+
+for indice, mascota in enumerate(mascotas):
+    print(indice, mascota)    
