1e9411b1d8ac58f892355267c54e0037f6cb4424
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-24 10:16:26 -0400

    buscando elementos

---
+++ tipos-avanzados/05-buscar-elemento.py
@@ -0,0 +1,8 @@
+mascotas = ["Pelusa", "Pulga", "Felipe", "Terry", "Kike"]
+
+print(mascotas.count("Pelusa"))
+print(mascotas.index("Terry"))
+
+if "Terry" in mascotas:
+    print(mascotas.index("Terry"))
+    
