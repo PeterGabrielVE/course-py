bf869cfbd750f77694dca44bb2762f720671fc7b
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-01 22:47:32 -0400

    constructor

---
+++ clases/03-constructor.py
@@ -0,0 +1,16 @@
+class Perro:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    
+    def habla(self):
+        print(f"{self.edad} dice : Guau")
+
+mi_perro = Perro("Pepe", 1)
+mi_perro2 = Perro("Pepita", 2)
+
+print(mi_perro.nombre)
+print(mi_perro2.nombre)
+
+print(mi_perro.habla())
+print(mi_perro2.habla())
