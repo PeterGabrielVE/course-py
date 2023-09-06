14ce34539d55eb2ed57c39f1d8ea470c52c1bfa2
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-06 11:40:32 -0400

    metodos magicos

--- clases/08-metodos-magicos.py
+++ clases/08-metodos-magicos.py
@@ -6,6 +6,9 @@ class Perro:
     def __str__(self):
         return f"Clase Perro: {self.nombre}"
 
+    def __del__(self):
+        print(f"Chao perro :( ")
+        
     def habla(self):
        print(f"{self.nombre} dice: Gaua!!")
 
@@ -13,3 +16,6 @@ class Perro:
 perro1 = Perro("titi", 5)
 
 print(perro1)
+
+perro2 = Perro("titina", 8)
+del perro2
