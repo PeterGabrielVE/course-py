dc8a6fb77b1248f2d819fdb97a50cfcde8ceb7b8
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-16 10:29:55 -0400

    else y fianlly

---
+++ excepciones/03-else-finally.py
@@ -0,0 +1,8 @@
+try:
+    n1 = int(input("ingresa primer numero:"))
+except:
+    print("Ocurrio un error :/ ")
+else:
+    print("No ocurrió ningun error")
+finally:
+    print("Se ejecuta siempre, thanks God")
