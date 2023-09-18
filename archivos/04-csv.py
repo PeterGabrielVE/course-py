a664007e1030eea33704f6d5db2a68163f9147da
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-18 11:42:45 -0400

    csv

---
+++ archivos/04-csv.py
@@ -0,0 +1,15 @@
+import csv 
+
+#with open("archivo.csv","w",newline="") as archivo:
+#    writer = csv.writer(archivo)
+#    writer.writerow(["twit_id", "user_id", "text"])
+#    writer.writerow([1000, 506, "este es un tuit"])
+#    writer.writerow([1001, 504, "otro tuit"])
+
+#leer
+#with open("archivo.csv") as archivo:
+#   reader = csv.reader(archivo)
+#   print(list(reader))
+#   archivo.seek(0)
+#   for linea in reader:
+#        print(linea)
