31528c25a500c6f80519f4a4b10df2f420f6ab0a
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-26 17:16:21 -0400

    intereactuando con la base de datos

--- productly/productos/views.py
+++ productly/productos/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hola Mundo!!!')
+    productos = Producto.objects.all().values()
+    #productos = Producto.objects.filter(puntaje=3)
+    #productos = Producto.objects.get(pk=1)
+    #return HttpResponse('hola mundo')
+    return JsonResponse(list(productos), safe=False)
