9886406fea2949de4086f7eca8dfc48688ff7eb4
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-26 17:04:08 -0400

    interactuando con la base de datos

--- productly/productos/models.py
+++ productly/productos/models.py
@@ -5,9 +5,15 @@ from django.utils import timezone
 class Categoria(models.Model):
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nombre
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     stock = models.IntegerField()
     puntaje = models.FloatField()
     categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE)
     creado_en = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre
