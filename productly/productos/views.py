bb543c5476e5c80d33a335159b566468ad05221c
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-27 11:38:37 -0400

    error 404

--- productly/productos/views.py
+++ productly/productos/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 # Create your views here.
@@ -16,7 +16,11 @@ def index(request):
 
 def detalle(request, producto_id):
     #return HttpResponse(producto_id)
-    producto = Producto.objects.get(id=producto_id)
-    return render(request,
-                'detalle.html',
-                context={ 'producto': producto })
+    #try:
+        producto = get_object_or_404(Producto,id=producto_id)
+        #producto = Producto.objects.get(id=producto_id)
+        return render(request,
+                    'detalle.html',
+                    context={ 'producto': producto })
+    #except Producto.DoesNotExist:
+        #raise Http404()
