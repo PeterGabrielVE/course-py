2caca08d19acf3fde556a21ef1c5d1c576bb1034
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-27 11:46:41 -0400

    links

--- productly/productos/urls.py
+++ productly/productos/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'productos'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:producto_id>',
     views.detalle,
-    name='producto_detalle')
+    name='detalle')
 ]
